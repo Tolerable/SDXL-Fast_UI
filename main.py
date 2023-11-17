005de29cfc4c33aab842283ae61bdd39be828af5
Reverend Dr Tolerant <2303210+Tolerable@users.noreply.github.com>
2023-11-17 11:36:57 -0500

    Menu Option now has Hide/Show all, needs sorting fixed.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import base64
 import tempfile
 import subprocess
 import win32clipboard
+import json
 
 # Global variable declarations
 img, label = None, None
@@ -20,10 +21,28 @@ last_prompt, last_ar, last_steps, last_upscale, last_seed, last_refiner = None, 
 image_generated, last_generated_image_path = False, None
 seed_entry, prompt_entry, aspect_ratio_var, upscale_var, steps_var, seed_var = None, None, None, None, None, None
 root, output_folder = None, None
+show_seed_var, show_refiner_var, show_steps_var, show_aspect_ratio_var, show_upscale_var = None, None, None, None, None
+
 
 # Initialize the Stable Diffusion pipeline
 pipe = StableDiffusionXLPipeline.from_pretrained("segmind/SSD-1B", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
 pipe.to("cuda")
+
+# Add global variables for UI elements that can be shown/hidden
+seed_frame, refiner_frame, steps_frame, aspect_ratio_frame, upscale_frame = None, None, None, None, None
+
+
+# Add the save and load settings functions
+def save_settings(settings):
+    with open('settings.json', 'w') as f:
+        json.dump(settings, f)
+
+def load_settings():
+    try:
+        with open('settings.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
 
 def update_image(filename, is_upscaled, is_default_image=False):
     global img, label
@@ -34,7 +53,8 @@ def update_image(filename, is_upscaled, is_default_image=False):
     label.configure(image=img)
     label.image = img
 
-def generate_image(prompt, width, height, steps, seed=None, use_refiner=False):
+def generate_image(prompt, width, height, steps, seed, use_refiner):
+    print(f"Generating image with refiner: {'On' if use_refiner else 'Off'}")
     if seed is not None:
         response = pipe(prompt=prompt, width=width, height=height, num_inference_steps=steps, generator=torch.Generator("cuda").manual_seed(seed), use_refiner=use_refiner)
     else:
@@ -45,29 +65,38 @@ def generate_image(prompt, width, height, steps, seed=None, use_refiner=False):
 def handle_user_input(event=None, new_generation=True):
     global last_prompt, last_ar, last_steps, last_upscale, last_seed, last_refiner, image_generated, last_generated_image_path
     
-    # Get the prompt text and strip leading/trailing whitespace
-    prompt = prompt_entry.get("1.0", "end-1c").strip()
+    if new_generation:
+        prompt = prompt_entry.get("1.0", "end-1c").strip()
+        if not prompt:
+            return  # Exit if the prompt is empty
 
-    # Check if the prompt is empty
-    if not prompt:
-        return  # Do nothing if the prompt is empty or only contains whitespace
-        
-    # Check if refiner_var is initialized
-    if refiner_var is None:
-        return  # Exit the function if refiner_var is not yet available
+        # Update last_* variables with current settings
+        last_prompt = prompt
+        last_ar = aspect_ratio_var.get() or '1'
+        last_upscale = upscale_var.get() == 'y'
+        steps_input = steps_var.get()
+        last_steps = int(steps_input) if steps_input.isdigit() else 20
+        seed_input = seed_var.get()
+        last_seed = int(seed_input) if seed_input.isdigit() else random.randint(0, 999999999999)
+        last_refiner = refiner_var.get() if refiner_var is not None else 'n'
 
-    use_refiner = refiner_var.get() == 'y'
+        # Set current settings based on UI input
+        use_refiner = last_refiner == 'y'
+    else:
+        # Redo operation: Reuse the last used settings
+        prompt = last_prompt
+        use_refiner = last_refiner == 'y'
     
-    prompt = prompt_entry.get("1.0", "end-1c") if new_generation or last_prompt is None else last_prompt
-    ar = aspect_ratio_var.get() or '1'
-    upscale = upscale_var.get() == 'y'
-    steps_input = steps_var.get()
-    steps = int(steps_input) if steps_input.isdigit() else 20 if new_generation or last_steps is None else last_steps
-    seed_input = seed_var.get()
-    seed = int(seed_input) if seed_input.isdigit() else random.randint(0, 999999999999) if new_generation or last_seed is None else last_seed
+    ar = last_ar
+    upscale = last_upscale
+    steps = last_steps
+    seed = last_seed
+
+    # Debugging output
+    print(f"Redo Generation with parameters: Seed={seed}, Refiner={'Yes' if use_refiner else 'No'}, Resolution 2x={'Yes' if upscale else 'No'}")
 
 
-    base_width, base_height = 512, 512  
+    base_width, base_height = 512, 512
     if ar == '2':
         base_width, base_height = 672, 384
     elif ar == '3':
@@ -125,12 +154,77 @@ def on_enter_key(event):
 	
     prompt_entry.bind("<Return>", on_enter_key)
 
+def load_settings():
+    settings_file = 'settings.json'
+    if os.path.exists(settings_file):
+        with open(settings_file, 'r') as file:
+            return json.load(file)
+    return {}
+
+def save_settings(settings):
+    with open('settings.json', 'w') as file:
+        json.dump(settings, file)
+
+def toggle_element(element, is_shown):
+    settings = load_settings()
+    settings[element] = is_shown
+    save_settings(settings)
+    update_ui_elements()
+
+def update_ui_elements():
+    global seed_frame, refiner_frame, steps_frame, aspect_ratio_frame, upscale_frame
+
+    # Check the state of each BooleanVar and update the UI elements
+    if show_seed_var.get():
+        seed_frame.pack(fill='x', padx=(10, 10), pady=5) if not seed_frame.winfo_viewable() else None
+    else:
+        seed_frame.pack_forget() if seed_frame.winfo_viewable() else None
+
+    if show_refiner_var.get():
+        refiner_frame.pack(side=tk.LEFT, padx=(30, 10), pady=5) if not refiner_frame.winfo_viewable() else None
+    else:
+        refiner_frame.pack_forget() if refiner_frame.winfo_viewable() else None
+
+    if show_steps_var.get():
+        steps_frame.pack(side=tk.LEFT, padx=(35, 10)) if not steps_frame.winfo_viewable() else None
+    else:
+        steps_frame.pack_forget() if steps_frame.winfo_viewable() else None
+
+    if show_aspect_ratio_var.get():
+        aspect_ratio_frame.pack(fill='x', padx=(70, 10)) if not aspect_ratio_frame.winfo_viewable() else None
+    else:
+        aspect_ratio_frame.pack_forget() if aspect_ratio_frame.winfo_viewable() else None
+
+    if show_upscale_var.get():
+        upscale_frame.pack(fill='x', padx=(70, 10)) if not upscale_frame.winfo_viewable() else None
+    else:
+        upscale_frame.pack_forget() if upscale_frame.winfo_viewable() else None
+   
+def toggle_all_elements():
+    current_state = show_all_var.get()
+    new_state = not current_state
+    show_all_var.set(new_state)
+
+    print(f"toggle_all_elements called: current_state={current_state}, new_state={new_state}")
+
+    show_seed_var.set(new_state)
+    show_refiner_var.set(new_state)
+    show_steps_var.set(new_state)
+    show_aspect_ratio_var.set(new_state)
+    show_upscale_var.set(new_state)
+
+    update_ui_elements()
+
+
+
+
 def run_tkinter():
     global root, prompt_entry, aspect_ratio_var, upscale_var, steps_var, seed_var, refiner_var, output_folder, label, last_prompt, last_ar, last_steps, last_upscale, last_seed, image_generated, last_generated_image_path
-    root = tk.Tk()
-    refiner_var = tk.StringVar(value='n')  # Now correctly initialize refiner_var as a global variable
-    root.title("Image Viewer")
+    global seed_frame, refiner_frame, steps_frame, aspect_ratio_frame, upscale_frame, show_all_var, show_seed_var, show_refiner_var, show_steps_var, show_aspect_ratio_var, show_upscale_var
 
+    root = tk.Tk()
+    refiner_var = tk.StringVar(value='n')
+    root.title("Image Viewer")
     output_folder = './OUTPUT'
     default_image = 'output_20231107-144208.jpg'
     script_directory = os.path.dirname(os.path.realpath(__file__))
@@ -148,8 +242,25 @@ def run_tkinter():
     # Display default image on startup if it exists
     if os.path.exists(default_image_path):
         update_image(default_image_path, False, is_default_image=True)
-    else:
-        first_image_displayed = False
+
+    # Create a menu bar
+    menu_bar = tk.Menu(root)
+    root.config(menu=menu_bar)
+    
+    # Create an "Options" menu
+    options_menu = tk.Menu(menu_bar, tearoff=0)
+    menu_bar.add_cascade(label="Options", menu=options_menu)
+    
+    # Load settings and determine the initial state for show_all_var
+    settings = load_settings()
+    initial_state = all(settings.get(key, True) for key in ['show_seed', 'show_refiner', 'show_steps', 'show_aspect_ratio', 'show_upscale'])
+    show_all_var = tk.BooleanVar(value=initial_state)
+
+    # Add the "Show/Hide All" checkbutton to the options menu
+    options_menu.add_checkbutton(label="Show/Hide All", onvalue=1, offvalue=0, variable=show_all_var, command=toggle_all_elements)
+
+    # Checkboxes within the Options menu to toggle UI elements
+    show_all_var = tk.BooleanVar(value=True)  # Default value can be True or False
 
     # User input fields and buttons
     # Prompt Input Frame
@@ -240,6 +351,20 @@ def run_tkinter():
     right_click_menu.add_command(label="Copy Image", command=copy_image_to_clipboard)
 
     label.bind("<Button-3>", lambda event: right_click_menu.post(event.x_root, event.y_root))
+
+    # Checkboxes within the Options menu to toggle UI elements
+    show_seed_var = tk.BooleanVar(value=load_settings().get('show_seed', True))
+    options_menu.add_checkbutton(label="Show Seed", onvalue=1, offvalue=0, variable=show_seed_var, command=lambda: toggle_element('show_seed', show_seed_var.get()))
+    show_refiner_var = tk.BooleanVar(value=load_settings().get('show_refiner', True))
+    options_menu.add_checkbutton(label="Show Refiner", onvalue=1, offvalue=0, variable=show_refiner_var, command=lambda: toggle_element('show_refiner', show_refiner_var.get()))
+    show_steps_var = tk.BooleanVar(value=load_settings().get('show_steps', True))
+    options_menu.add_checkbutton(label="Show Steps", onvalue=1, offvalue=0, variable=show_steps_var, command=lambda: toggle_element('show_steps', show_steps_var.get()))
+    show_aspect_ratio_var = tk.BooleanVar(value=load_settings().get('show_aspect_ratio', True))
+    options_menu.add_checkbutton(label="Show Aspect Ratio", onvalue=1, offvalue=0, variable=show_aspect_ratio_var, command=lambda: toggle_element('show_aspect_ratio', show_aspect_ratio_var.get()))
+    show_upscale_var = tk.BooleanVar(value=load_settings().get('show_upscale', True))
+    options_menu.add_checkbutton(label="Show Upscale", onvalue=1, offvalue=0, variable=show_upscale_var, command=lambda: toggle_element('show_upscale', show_upscale_var.get()))
+
+    update_ui_elements()  # Apply the settings to the UI
 
     root.mainloop()
 
